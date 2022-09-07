4c34b88c9f36c3fa99620e39cd8a3830d50e8957
Bin-08-01 <minhtri192035@gmail.com>
2022-09-07 11:57:15 +0700

    Add an image with the extension .gif

--- commands/normalCommands.js
+++ commands/normalCommands.js
@@ -5,6 +5,7 @@ const { AttachmentBuilder } = require('discord.js');
 const normalCommands = {
     normalCom: (client) => {
         client.on('messageCreate', async (msg) => {
+            console.log(msg.author.avatarURL());
             // const user = await client.users.fetch(msg.author.id);
             const commandName = msg.content;
             if (commandName.toLowerCase() === '$hello') {
@@ -18,14 +19,20 @@ const normalCommands = {
                 await msg.reply(`Server name: ${msg.guild.name}\nTotal members: ${msg.guild.memberCount}`);
             }
             else if (commandName === '$user') {
-                const canvas = Canvas.createCanvas(500, 500);
+                /*======== Use canvas to draw something ================
+                const canvas = Canvas.createCanvas(100, 100);
                 const context = canvas.getContext('2d');
-                const background = await Canvas.loadImage(`https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.webp`);
+                const background = await Canvas.loadImage(imageURL);
                 context.drawImage(background, 0, 0, canvas.width, canvas.height);
-                const attachment = new AttachmentBuilder(await canvas.encode('webp', 'p'), { name: 'avatar.png' });
+                const attachment = new AttachmentBuilder(await canvas.encode('webp', 'gif'), { name: 'avatar.gif' });
+                await msg.channel.send({ content: 'Hello', files: [attachment] });
+                const imageURL = 'https://cdn.discordapp.com/avatars/${msg.author.id}/${msg.author.avatar}.webp';
+                ======== ============================= ================*/
+                const imageURL = msg.author.avatarURL();
 
-                msg.reply(`Your name: ${msg.author.username}\nYour id: ${msg.author.id}\nYour avatar:`);
-                msg.reply({ files: [attachment] });
+
+                await msg.reply({ content: `Your name: ${msg.author.username}\nYour id: ${msg.author.id}\nYour avatar:`, files: [imageURL] });
+                // msg.reply({ files: [attachment] });
             }
             else if (commandName === '$dis') {
                 await msg.member.voice.disconnect();
